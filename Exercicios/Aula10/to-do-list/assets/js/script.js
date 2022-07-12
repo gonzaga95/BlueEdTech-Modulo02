c5cdf86b381af17ab5e03243278485848b82a887
Carlos Eduardo Gonzaga <91717487+gonzaga95@users.noreply.github.com>
2022-07-11 20:06:52 -0400

    botao remover tarefa

--- Exercicios/Aula10/to-do-list/assets/js/script.js
+++ Exercicios/Aula10/to-do-list/assets/js/script.js
@@ -8,8 +8,26 @@ addBtn.addEventListener("click", function () {
     li.appendChild(textTask);
 
     if (inputValue === "") {
-        alert('Digite uma tarefa');
+        alert("Digite uma tarefa");
     } else {
         document.getElementById("myUL").appendChild(li);
+    }
+
+    document.querySelector("#myInput").value = "";
+
+    const span = document.createElement("span");
+    const icon = document.createTextNode("\u00D7");
+
+    span.className = "close";
+    span.appendChild(icon);
+    li.appendChild(span);
+
+    const close = document.querySelectorAll(".close");
+
+    for (let i = 0; i < close.length; i++) {
+        close[i].addEventListener("click", function () {
+            const li = this.parentElement;
+            li.style.display = "none";
+        });
     }
 });
