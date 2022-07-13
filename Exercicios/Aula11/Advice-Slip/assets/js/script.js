947e8146703f1d974e087518d254da2ee60ca195
Carlos Eduardo Gonzaga <91717487+gonzaga95@users.noreply.github.com>
2022-07-13 19:57:05 -0400

    projeto pronto

--- Exercicios/Aula11/Advice-Slip/assets/js/script.js
+++ Exercicios/Aula11/Advice-Slip/assets/js/script.js
@@ -1,3 +1,13 @@
 async function getAdvice() {
-    
+    const resposta = await fetch("https://api.adviceslip.com/advice");
+
+    const data = await resposta.json();
+
+    document.querySelector("#advice").innerText = data.slip.advice;
 }
+
+const botaoGetAdvice = document.querySelector("#get-advice");
+
+botaoGetAdvice.addEventListener("click", function () {
+    getAdvice();
+});
