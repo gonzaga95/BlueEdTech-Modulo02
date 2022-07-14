b8d1bfb82731d7b4b968c5e282878020ae39d2e6
Carlos Eduardo Gonzaga <91717487+gonzaga95@users.noreply.github.com>
2022-07-13 21:57:33 -0400

    fn consumir api e fn carregar pagina infinita

--- Exercicios/Aula13/script.js
+++ Exercicios/Aula13/script.js
@@ -1,7 +1,44 @@
+let page = 1;
+
 async function getCharacters () {
-    const resposta = await fetch("https://rickandmortyapi.com/api/character");
+    const resposta = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`);
 
     const data = await resposta.json();
 
-    
+    data.results.forEach(character => {
+        document.querySelector("#character-list").insertAdjacentHTML('beforeend', `
+        <div class="card">
+            <img class="character" src="${character.image}"
+                alt="Avatar do ${character.name}">
+
+            <div>
+                <h2 class="name-character">${character.name}</h2>
+                <p class="spec-character">${character.species}</p>
+                
+                <h4>Gender</h4>
+                <p class="spec-character">${character.gender}</p>
+
+                <h4>Origin</h4>
+                <p class="spec-character">${character.origin.name}</p>
+
+            </div>
+
+        </div>
+        `)
+    })
 }
+
+getCharacters();
+
+function viewMore(){
+    page++;
+    getCharacters();
+}
+
+window.addEventListener('scroll', function () {
+    const {scrollTop, scrollHeight, clientHeight} = document.documentElement;
+
+    if (scrollTop + clientHeight >= scrollHeight) {
+        viewMore();
+    }
+})
