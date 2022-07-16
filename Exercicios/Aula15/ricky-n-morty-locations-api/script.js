1ddc20d7cabf6ba8494492368af3363d779f3601
Carlos Eduardo Gonzaga <91717487+gonzaga95@users.noreply.github.com>
2022-07-15 21:35:22 -0400

    mouse over fn

--- Exercicios/Aula15/ricky-n-morty-locations-api/script.js
+++ Exercicios/Aula15/ricky-n-morty-locations-api/script.js
@@ -1,6 +1,61 @@
 let page = 1;
 const locationsList = document.querySelector("#locations-list");
 
-async function getLocations () { 
-    const resp = await fetch (`https://rickandmortyapi.com/api/location?page=${page}`);
+async function getLocations() {
+    const resp = await fetch(
+        `https://rickandmortyapi.com/api/location?page=${page}`
+    );
+
+    const data = await resp.json();
+
+    data.results.forEach((location) => {
+        locationsList.insertAdjacentHTML(
+            "beforeend",
+            `
+        <div class="card">
+        <img class="planet" src="https://thumbs.dreamstime.com/b/mundo-terra-do-planeta-modelo-global-isolado-no-fundo-branco-elementos-desta-imagem-fornecidos-pela-nasa-136878303.jpg">
+        <h2>${location.name}</h2>
+        
+        <div class="details">
+        
+        <h4>ID</h4>
+        <p>${location.id}</p>
+
+        <h4>Tipo</h4>
+        <p>${location.type}</p>
+        
+        <h4>Dimensão</h4>
+        <p>${location.dimension}</p>
+
+        <h4>População</h4>
+        <p>${location.residents.length}</p>
+
+        </div>
+        
+        </div>
+        `
+        );
+    });
+
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach((card) => {
+        card.addEventListener("mouseenter", function (event) {
+            getCardElemento(event).style.display = "block";
+        });
+
+        card.addEventListener("mouseleave", function (event) {
+            getCardElemento(event).style.display = "none";
+        })
+    });
+
+    function getCardElemento(evento) {
+        const cardElement = evento.path.filter(
+            (item) => item.className === "card"
+        );
+        const details = cardElement[0].children[2];
+        return details;
+    }
 }
+
+getLocations();
